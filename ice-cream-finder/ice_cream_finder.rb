2e7935ea2792ff22763faf00a2fa2df7b731c9d2
App Academy Student <student@appacademy.io>
2013-06-03 14:41:36 -0700

    rename file, cleanup code

--- ice-cream-finder/ice_cream_finder.rb
+++ ice-cream-finder/ice_cream_finder.rb
@@ -11,11 +11,10 @@ class IceCreamFinder
   TYPE = "food"
 
   def initialize(origin)
-    # origin = "160 Folsom Street, San Francisco"
     @origin = origin
   end
 
-  def find_ice_cream
+  def nearby_directions
     lat, lng = convert_to_coords(origin)
     places = nearby_places(lat, lng)
 
@@ -23,21 +22,10 @@ class IceCreamFinder
       puts "#{name}:"
       directions = directions_str(origin, destination)
       puts "#{directions}"
-      # legs = routes[0]["legs"]
-      # legs.each do |leg|
-      #   steps = leg["steps"]
-      #   steps.each do |step|
-      #     html = step["html_instructions"]
-      #     div = '<div style="font-size:0.9em">'
-      #     html = html.gsub(div, "#{div}\n")
-      #     parsed_html = Nokogiri::HTML(html)
-      #     puts parsed_html.text
-      #     # puts html
-      #   end
-      # end
     end
   end
 
+  private
   def convert_to_coords(address)
     url = Addressable::URI.new(
        :scheme => "http",
@@ -79,8 +67,6 @@ class IceCreamFinder
       name = result["name"]
       address = result["vicinity"]
       destinations[name] = address
-      # puts result["name"]
-      # destinations << result["vicinity"]
     end
     destinations
   end
@@ -109,62 +95,11 @@ class IceCreamFinder
          html = html.gsub(div, "#{div}\n\t")
          parsed_html = Nokogiri::HTML(html)
          directions << "\t#{parsed_html.text}"
-       # puts html
       end
     end
     directions.join"\n"
   end
-
 end
 
 finder = IceCreamFinder.new("160 Folsom Street, San Francisco")
-finder.find_ice_cream
-
-
-
-# puts url
-
-# url = "http://maps.googleapis.com/maps/api/geocode/json?address=160+folsom+street,+san+francisco&sensor=true"
-
-
-# puts lat
-# puts lng
-
-# puts json["results"]["geometry"]
-
-# keyword = "ice cream"
-# type = "food"
-#
-# url = Addressable::URI.new(
-#    :scheme => "https",
-#    :host => "maps.googleapis.com",
-#    :path => "maps/api/place/nearbysearch/json",
-#    :query_values => {
-#       :location => "#{lat},#{lng}",
-#       :radius => 500,
-#       :sensor => false,
-#       :keyword => keyword,
-#       :type => type,
-#       :key => API_KEY
-#     }
-#  ).to_s
-#
-# response = RestClient.get(url)
-# json = JSON.parse(response)
-#
-# results = json["results"]
-# results.each do |result|
-#   puts result["name"]
-#   destination = result["vicinity"]
-#   puts "Directions:"
-#   show_directions(origin, destination)
-#   puts
-# end
-#
-# destination = results[0]["vicinity"]
-
-# http://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&sensor=false
-
-
-# directions =
-# puts json
+finder.nearby_directions
