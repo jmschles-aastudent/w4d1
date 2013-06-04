f1ea6b0ccb83f5a1afc49db82cc95ed8e33c5948
App Academy Student <student@appacademy.io>
2013-06-03 17:25:37 -0700

    method to get entire followers_list

--- twitter-client/twitter_session.rb
+++ twitter-client/twitter_session.rb
@@ -6,7 +6,6 @@ require 'rest-client'
 
 # "consumer" in OAuth terminology means "client" in our discussion.
 
-
 class TwitterSession
   CONSUMER_KEY = "iEF943evwJMAHlTsfaf1ew"
   CONSUMER_SECRET = "6k7kA6mqBrCqkc5i6bvuzONdBEtuhErssENw7FnXXbw"
@@ -22,61 +21,24 @@ class TwitterSession
   def request_access_token
     return access_token if access_token
 
-    # access_token = access_token_from_file("token")
-   #  return access_token if access_token
-
-    # send user to twitter URL to authorize application
     request_token = CONSUMER.get_request_token
     authorize_url = request_token.authorize_url
     puts "Go to this URL: #{authorize_url}"
     Launchy.open(authorize_url)
     begin
-      # because we don't use a redirect URL; user will receive an "out of
-      # band" verification code that the application may exchange for a
-      # key; ask user to give it to us
       puts "Login, and type your verification code in"
       oauth_verifier = gets.chomp
-
-      # ask the oauth library to give us an access token, which will allow
-      # us to make requests on behalf of this user
       access_token = request_token.get_access_token(
           :oauth_verifier => oauth_verifier)
     rescue
       puts "Wrong code"
       retry
     end
-    # File.open("token", 'w') {|f| f.write(access_token) }
-    # @access_token = access_token
     access_token
   end
 
-  # fetch a user's timeline
-  def user_timeline #(access_token)
-    url = Addressable::URI.new(
-       :scheme => "http",
-       :host => "api.twitter.com",
-       :path => "1.1/statuses/user_timeline.json"
-       ).to_s
-
-    raw_json = access_token.get(url).body
-    json = JSON.parse(raw_json)
-    json
-  end
-
-  def tweets
-    timeline = user_timeline
-    tweets = []
-    # timeline = session.user_timeline
-    timeline.each do |tweet|
-      tweets << tweet["text"]
-    end
-    tweets
-  end
-
-  def set_token(token_file)
-    # We can serialize token to a file, so that future requests don't need
-    # to be reauthorized.
-
+  def set_token(username)
+    token_file = "tokens/#{username}.yml"
     if File.exist?(token_file)
       @access_token = File.open(token_file) { |f| YAML.load(f) }
     else
@@ -88,8 +50,10 @@ class TwitterSession
 
 end
 
-token_file = "token.yml"
-session = TwitterSession.new
-session.set_token(token_file)
-tweets = session.tweets
-p tweets
+if __FILE__ == $0
+  token_file = "token.yml"
+  session = TwitterSession.new
+  session.set_token(token_file)
+  tweets = session.tweets
+  p tweets
+end
---
+++ twitter-client/user.rb
@@ -0,0 +1,115 @@
+require_relative 'twitter_session'
+require 'addressable/uri'
+
+class User
+  attr_reader :username, :session
+
+  def initialize(username)
+    @username = username
+    @session = nil
+    # @access_token = nil
+  end
+
+  def set_access_token
+    @session = TwitterSession.new
+    session.set_token(username)
+    puts "access token set: #{session.access_token}"
+  end
+
+  def check_access_token
+    url = Addressable::URI.new(
+       :scheme => "http",
+       :host => "api.twitter.com",
+       :path => "1/account/verify_credentials.json"
+       ).to_s
+
+    session.access_token.get(url).response.code
+  end
+
+  # fetch a user's timeline
+  def timeline #(access_token)
+    url = Addressable::URI.new(
+       :scheme => "http",
+       :host => "api.twitter.com",
+       :path => "1.1/statuses/user_timeline.json"
+       ).to_s
+
+    raw_json = session.access_token.get(url).body
+    json = JSON.parse(raw_json)
+    json
+  end
+
+  def tweets
+    tweets = []
+    timeline.each do |tweet|
+      tweets << tweet["text"]
+    end
+    tweets
+  end
+
+  def followers_ids(for_username = username)
+    url = Addressable::URI.new(
+    :scheme => "https",
+    :host => "api.twitter.com",
+    :path => "1.1/followers/ids.json",
+    :query_values => {
+      :screen_name => for_username,
+      :count => 5000
+      }).to_s
+
+    raw_json = session.access_token.get(url).body
+    json = JSON.parse(raw_json)
+    json
+  end
+
+  def followers_list(for_username = username)
+    followers_list = []
+    cursor = -1
+    until cursor.zero?
+      url = Addressable::URI.new(
+      :scheme => "https",
+      :host => "api.twitter.com",
+      :path => "1.1/followers/list.json",
+      :query_values => {
+        :screen_name => for_username,
+        :skip_status => true,
+        :include_user_entities => false,
+        :cursor => cursor
+      }).to_s
+
+      # puts session.access_token
+      # p session.access_token.get(url).header
+      # break
+      raw_json = session.access_token.get(url).body
+      json = JSON.parse(raw_json)
+
+      puts cursor
+      # puts json
+      users = json["users"]
+      users.each do |user|
+        followers_list << user["screen_name"]
+      end
+
+      cursor = json["next_cursor"]
+    end
+    followers_list
+  end
+
+  def followers_usernames(for_username = username)
+    json = followers_list(for_username)
+    users = json["users"]
+    usernames = []
+    users.each do |user|
+      usernames << user["screen_name"]
+    end
+    usernames
+  end
+
+end
+
+sean = User.new("seanomlor")
+sean.set_access_token
+# puts sean.followers_list
+p sean.followers_list
+# p sean.check_access_token
+# sean.follower_names
